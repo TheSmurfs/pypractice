e38335c7d96cf8d1c036317806fd6c0e7bbd0b85
方长青 <752105005@qq.com>
2018-04-07 23:21:24 +0800

    .

--- week1/三级菜单/3level.py
+++ week1/三级菜单/3level.py
@@ -7,16 +7,16 @@
 with open("menu.txt","r",encoding="utf-8") as f:
     f_str = f.read()     #将文件内容转换为字符串
     #print(f_str)
-    file_str = str(f_str)  # 将每行信息转成字符串格式
-    type(file_str)
-    #menu = eval(f.readline())    #将字符串转换为字典
-    menu = eval(file_str.strip())
+    menu = eval(f_str)  #将字符串转换为字典
+    #print(menu)
+print("\033[31;1m按q退出程序，按b后退上一级目录。\033[0m")
+
 exit_flag = False
 while not exit_flag:
     for i in menu:
         print(i)
-
     choice = input("请选择省份直辖市:")
+
     if choice in menu:
         while not exit_flag:
             for i2 in menu[choice]:
@@ -28,18 +28,30 @@ while not exit_flag:
                         print(i3)
                     choice3 = input("请选择小地名>>>")
                     if choice3 in menu[choice][choice2]:
-                        for i4 in menu[choice][choice2][choice3]:
-                            print(i4)
-                        choice4 = input("最后一层>>>按b退出")
-                        if choice4== "b":
-                            pass
-                        elif choice4 == "q":
-                            exit_flag = True
-                    if choice3 == "b":
-                        break
+                        while not exit_flag:
+                            for i4 in menu[choice][choice2][choice3]:
+                                print(i4)
+                            choice4 = input("最后一层>>>按b重新回到初始页")
+                            if choice4 == "b":
+                                break
+                            elif choice4 == "q":
+                                exit_flag = True
+                    elif choice3 == "b":
+                            break
                     elif choice3 == "q":
-                        exit_flag = True
+                            exit_flag = True
             if choice2 == "b":
-                 break
+                #print("1")
+                break
             elif choice2 == "q":
                 exit_flag = True
+    else:
+        break
+
+
+#def operation(choose):
+#    if choose == "b":
+#        break
+#    elif choose == "q":
+#       exit_flag = True
+
--- week1/三级菜单/test.py
+++ week1/三级菜单/test.py
@@ -1,13 +1,54 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-# author:FCQ
-# datetime:2018/4/5 22:49
-# software: PyCharm
-
-with open("menu.txt","r",encoding="utf-8") as f:
-    f_str = f.read()     #将文件内容转换为字符串
-    print(f_str)
-    file_str = str(f_str)  # 将每行信息转成字符串格式
-    type(file_str)
-    #menu = eval(f.readline())    #将字符串转换为字典
-    #menu = eval(file_str)
+#/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# Auther:yooma 2016-08-14 22:00
+import sys
+execLaye = {1:{"北京":{1:{"东城":{1:{"建国门":{1:"建国门大厦",2:"门建国大厦"}},2:{"东单":{1:"东单大厦",2:"单东大厦"}}}
+                         },
+                       2:{"西城":{1:{"阜成门":{1:"四川大厦",2:"中基大厦"}},2:{"西直门":{1:"沸腾鱼",2:"西直门桥"}}}
+                         },
+                      }
+              },
+            2:{"上海":{1:{"浦东":{1:{"浦东一":{1:"浦东一大厦",2:"浦东一一大厦"}},2:{"浦东二":{1:"浦东二大厦",2:"浦东二二大厦"}}}
+                         },
+                       2:{"东环":{1:{"东环一":{1:"东环一大厦",2:"东环一一大厦"}},2:{"东环二":{1:"东环二大厦",2:"东环二二大厦"}}}
+                         }
+                      },
+              },
+           }
+def help():
+    print('''
+          Useage:
+                 1. [ h ]  -- help to useaga.
+                 2. [ 1 | 2 ] -- Input city number selected [1 or 2] into it.
+                 3. [ b ]  -- "b" parameter function is to return to the previous level menu.
+                 4. [ q | quit | exit ] -- function is exit current program.
+          ''')
+def select(value, record):
+    city = ""
+    for k, v in value.items():
+        if isinstance(v,str):
+            city = city + '\t' + str(k) + '.' + ' %s\n' % v
+        else:
+            city = city + '\t' + str(k) + '.' + ' %s\n' % list(v.keys())[0]
+#    SelectCity = input("%sSelect please: " % city)
+    while 1:
+        SelectCity = input("%sSelect please: " % city)
+        if SelectCity == 'h':
+            help()
+        #    sys.exit(0)
+        for n, i in value.items():
+            if SelectCity == str(n):
+                if isinstance(i,str):
+                    print("~~~~~~~Welcome to %s ~~~~~~~" % i)
+                    sys.exit(0)
+                else:
+                    print("[ Welcome to %s ]" % list(i.keys())[0])
+                    select(list(i.values())[0], value)
+            elif SelectCity == 'b':
+                select(record, record)
+            elif SelectCity == 'q' or SelectCity == 'quit' or SelectCity == 'exit':
+                sys.exit(0)
+            else:
+                continue
+if __name__ == "__main__":
+    select(execLaye,execLaye)
